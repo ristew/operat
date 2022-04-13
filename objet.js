e119afb2f33da9f5fb10a3282465a7d54f863c96
Riley Stewart <me@rileystew.art>
2022-04-12 19:23:27 -0700

    makin a right big mess of things

--- objet.js
+++ objet.js
@@ -24,6 +24,126 @@ function proxyClass(clss) {
   })
 }
 
+function delegate(object, methodName, args) {
+  method = findMethod(object.class, methodName);
+}
+
+function combineMethod(oldMethod, newMethod) {
+}
+
+function findMethod(classObject, methodName) {
+  let method = null;
+  if (methodName in classObject.methods) {
+    method = classObject.methods[methodName];
+  }
+  return classObject.supers.reduce((prev, cur) => combineMethod(prev, findMethod(cur, methodName)), method);
+}
+
+
+function instantiate({ supers, slots }) {
+  let proto = {
+    addSlot(name, slotDef) {
+      // check if a slot is being shadowed
+      let cur = this[name];
+      if (cur) {
+        this[name] = { ...cur, ...slotDef };
+      } else if (slotDef.static) {
+        if (slotDefIsFn(slotDef)) {
+          this[name] = this.wrapFn(slotDef);
+        }
+      } else {
+        this.slots.push(name);
+        this[name] = slotDef;
+      }
+    },
+
+    mergeSuper(sup) {
+      for (let slot of sup.slots) {
+        this.addSlot(slot, sup[slot]);
+      }
+    },
+
+    instantiate(passedVals = {}) {
+      let o = {
+        meta: this,
+        id: genid(),
+      };
+      for (let slot of this.slots.filter(s => s !== 'methods')) {
+        let def = this[slot];
+        if (passedVals[slot]) {
+          o[slot] = passedVals[slot];
+        } else if (!def.optional && !('default' in def) && !slotDefIsFn(def)) {
+          throw new Error('Missing value in instantiation: ' + slot);
+        }
+      }
+      let jsproto = this.find('jsproto');
+      if (jsproto) {
+        o.__proto__ = jsproto;
+      }
+      return new Proxy(o, {
+        get(target, p, receiver) {
+          if (!(p in target)) {
+            let found = target.meta.find(p, receiver);
+            if (found === undefined) {
+              throw new Error("couldn't find " + p);
+            } else if (found !== null) {
+              target[p] = found;
+            }
+            // Be fussy on undefined?
+          }
+          return target[p];
+        }
+      });
+    },
+
+    find(name, target = this) {
+      if (name in this) {
+        let slotDef = this[name];
+        if ('default' in slotDef) {
+          return slotDef.default;
+        } else if (typeof slotDef === 'function') {
+          return slotDef;
+        } else {
+          console.log(slotDef);
+          throw new Error('found something wrong');
+        }
+      } else if (name in this.methods) {
+        this[name] = this.wrapFn(this.methods[name], target);
+        return this[name];
+      } else {
+        for (let sup of proto.supers) {
+          let found = sup.find(name, target);
+          if (found) {
+            return found;
+          }
+        }
+      }
+      return null;
+    },
+    call(method, ...args) {
+      let mfn = this.find(method).bind(this);
+    },
+    slots: [],
+    supers: [...supers.slice().reverse(), StandardClass],
+    type: 'object',
+  };
+  // linearize!
+  for (let sup of proto.supers) {
+    proto.mergeSuper(sup);
+  }
+  for (let [slotName, slotDef] of Object.entries(slots)) {
+    proto.addSlot(slotName, slotDef);
+  }
+
+  return proto;
+}
+
+/*
+ * addSlot
+ * mergeSuper
+ * instantiate
+ * findMethod
+ */
 export const StandardClass = {
   // fake metaclass
   slots: [],
@@ -33,120 +153,11 @@ export const StandardClass = {
   methods: {
     instantiate: {
       fn({ supers, slots }) {
-
+        instantiate({ supers, slots })
       }
-    }
+    },
+
   },
-  instantiate: function({ supers, slots }) {
-    let proto = {
-      wrapFn(def, bind = this) {
-        let fn = new Proxy(def.fn, {
-          apply(target, thisArg, args) {
-            return target.apply(bind, args);
-          }
-        });
-        for (let prop in def) {
-          if (prop !== 'fn') {
-            fn[prop] = def[prop];
-          }
-        }
-        return fn;
-      },
-      addSlot(name, slotDef) {
-        // check if a slot is being shadowed
-        let cur = this[name];
-        if (cur) {
-          this[name] = { ...cur, ...slotDef };
-        } else if (slotDef.static) {
-          if (slotDefIsFn(slotDef)) {
-            this[name] = this.wrapFn(slotDef);
-          }
-        } else {
-          this.slots.push(name);
-          this[name] = slotDef;
-        }
-      },
-
-      mergeSuper(sup) {
-        for (let slot of sup.slots) {
-          this.addSlot(slot, sup[slot]);
-        }
-      },
-
-      instantiate(passedVals = {}) {
-        let o = {
-          meta: this,
-          id: genid(),
-        };
-        for (let slot of this.slots.filter(s => s !== 'methods')) {
-          let def = this[slot];
-          if (passedVals[slot]) {
-            o[slot] = passedVals[slot];
-          } else if (!def.optional && !('default' in def) && !slotDefIsFn(def)) {
-            throw new Error('Missing value in instantiation: ' + slot);
-          }
-        }
-        let jsproto = this.find('jsproto');
-        if (jsproto) {
-          o.__proto__ = jsproto;
-        }
-        return new Proxy(o, {
-          get(target, p, receiver) {
-            if (!(p in target)) {
-              let found = target.meta.find(p, receiver);
-              if (found === undefined) {
-                throw new Error("couldn't find " + p);
-              } else if (found !== null) {
-                target[p] = found;
-              }
-              // Be fussy on undefined?
-            }
-            return target[p];
-          }
-        });
-      },
-
-      find(name, target = this) {
-        if (name in this) {
-          let slotDef = this[name];
-          if ('default' in slotDef) {
-            return slotDef.default;
-          } else if (typeof slotDef === 'function') {
-            return slotDef;
-          } else {
-            console.log(slotDef);
-            throw new Error('found something wrong');
-          }
-        } else if (name in this.methods) {
-          this[name] = this.wrapFn(this.methods[name], target);
-          return this[name];
-        } else {
-          for (let sup of proto.supers) {
-            let found = sup.find(name, target);
-            if (found) {
-              return found;
-            }
-          }
-        }
-        return null;
-      },
-      call(method, ...args) {
-        let mfn = this.find(method).bind(this);
-      },
-      slots: [],
-      supers: [...supers.slice().reverse(), StandardClass],
-      type: 'object',
-    };
-    // linearize!
-    for (let sup of proto.supers) {
-      proto.mergeSuper(sup);
-    }
-    for (let [slotName, slotDef] of Object.entries(slots)) {
-      proto.addSlot(slotName, slotDef);
-    }
-
-    return proto;
-  }
 }
 
 export function classDef($supers, $slots) {
