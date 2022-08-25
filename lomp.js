72a00fc6acb4cdebe3ed6558e958068320b7a63d
Riley Stewart <me@rileystew.art>
2022-08-24 22:54:14 -0700

    lomp.test with jest

--- lomp.js
+++ lomp.js
@@ -1,21 +1,51 @@
-function TypeSym(ctx, s) {
-  this._ctx = ctx;
-  this._str = s;
-}
+const context = {
+    'add-method-ref'(methodName, ref) {
+        let m = this.methods[methodName];
+        m.refs.push(ref);
+    },
+    'save-native-method'(methodName, fn) {
+        let oldm = this.methods[methodName];
+        fn.refs = [];
+        this.methods[methodName] = fn;
+        oldm?.refs.map(ref => ref[methodName] = fn.bind(ref));
+    },
+    methods: {
+    },
+};
+context.save_native_method('dist', function() { return Math.sqrt(this.x ** 2 + this.y ** 2); });
 
-function symgen(ctx, proto) {
-  return new Proxy({}, {
-    get(_, p, recv) {
-      return new proto(ctx, p);
+const Base = new Proxy({}, {
+    get(target, p, recv) {
+        if (p in target) {
+            return target[p];
+        } else if (p in context.methods) {
+            context.methods[p];
+            context.add_method_ref(p, recv);
+            target[p] = context.methods[p].bind(recv);
+            return target[p];
+        }
     }
-  });
+})
+
+function make(o) {
+    let obj = Object.create(Base);
+    for (let [key, val] of Object.entries(o)) {
+        obj[key] = val;
+    }
+    return obj;
 }
 
-function Context() {
-  this.csym = {};
-  this.types = symgen(this, TypeSym);
+let test = make({ x: 3, y: 4 });
+console.log(test.dist())
+context['#save-native-method']('cartesianDist', function () { return this.x + this.y; });
+console.log(test.cartesianDist());
+
+const oClass = {
+  constructor({ name }) {
+    this.name = name;
+    this.methods = {};
+  },
+  id() { return this.name; },
+  instantiate(props) {
+  }
 }
-
-
-let ctx = new Context();
-console.log(ctx.types.class);
